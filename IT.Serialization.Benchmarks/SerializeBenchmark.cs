8c7674aaf24c9305342eaf550aef70ac42d7b217
Ivan Tikhonov <itikhonov@mail.com>
2022-12-10 23:59:09 +0300

    add NETCOREAPP3_1_OR_GREATER

--- IT.Serialization.Benchmarks/SerializeBenchmark.cs
+++ IT.Serialization.Benchmarks/SerializeBenchmark.cs
@@ -16,30 +16,33 @@ public class SerializeBenchmark
 
     private readonly ISerialization _jsonSerializer;
     private readonly byte[] _jsonSerializerBytes;
-    private readonly ISerialization _memoryPackSerializer;
-    private readonly byte[] _memoryPackSerializerBytes;
     private readonly ISerialization _messagePackSerializer;
     private readonly byte[] _messagePackSerializerBytes;
     private readonly ISerialization _utf8jsonSerializer;
     private readonly byte[] _utf8jsonSerializerBytes;
 
+#if NETCOREAPP3_1_OR_GREATER
+    private readonly ISerialization _memoryPackSerializer;
+    private readonly byte[] _memoryPackSerializerBytes;
+#endif
+
     public SerializeBenchmark()
     {
         _jsonSerializer = new Json.TextSerialization();
         _jsonSerializerBytes = Json_Serialize();
-        _memoryPackSerializer = new MemoryPack.Serialization();
-        _memoryPackSerializerBytes = MemoryPack_Serialize();
         _messagePackSerializer = new MessagePack.Serialization();
         _messagePackSerializerBytes = MessagePack_Serialize();
         _utf8jsonSerializer = new Utf8Json.TextSerialization();
         _utf8jsonSerializerBytes = Utf8Json_Serialize();
+
+#if NETCOREAPP3_1_OR_GREATER
+        _memoryPackSerializer = new MemoryPack.Serialization();
+        _memoryPackSerializerBytes = MemoryPack_Serialize();
+#endif
     }
 
     [Benchmark]
     public byte[] Json_Serialize() => _jsonSerializer.Serialize(_person);
-
-    [Benchmark]
-    public byte[] MemoryPack_Serialize() => _messagePackSerializer.Serialize(_person);
 
     [Benchmark]
     public byte[] MessagePack_Serialize() => _messagePackSerializer.Serialize(_person);
@@ -51,9 +54,6 @@ public class SerializeBenchmark
     public Person? Json_Deserialize() => _jsonSerializer.Deserialize<Person>(_jsonSerializerBytes);
 
     [Benchmark]
-    public Person? MemoryPack_Deserialize() => _memoryPackSerializer.Deserialize<Person>(_memoryPackSerializerBytes);
-
-    [Benchmark]
     public Person? MessagePack_Deserialize() => _messagePackSerializer.Deserialize<Person>(_messagePackSerializerBytes);
 
     [Benchmark]
@@ -63,13 +63,23 @@ public class SerializeBenchmark
     public Person Json() => SerializeDeserialize(_jsonSerializer);
 
     [Benchmark]
-    public Person MemoryPack() => SerializeDeserialize(_memoryPackSerializer);
-
-    [Benchmark]
     public Person MessagePack() => SerializeDeserialize(_messagePackSerializer);
 
     [Benchmark]
     public Person Utf8Json() => SerializeDeserialize(_utf8jsonSerializer);
+
+#if NETCOREAPP3_1_OR_GREATER
+
+    [Benchmark]
+    public byte[] MemoryPack_Serialize() => _messagePackSerializer.Serialize(_person);
+
+    [Benchmark]
+    public Person? MemoryPack_Deserialize() => _memoryPackSerializer.Deserialize<Person>(_memoryPackSerializerBytes);
+
+    [Benchmark]
+    public Person MemoryPack() => SerializeDeserialize(_memoryPackSerializer);
+
+#endif
 
     private Person SerializeDeserialize(ISerialization serializer)
     {
